c1e700059462919c056998d1f1d2ca6ff90350f0
turgaykirkil <turgaykirkil@gmail.com>
2023-10-07 16:43:56 +0300

    Initial Commit

---
+++ foodGoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodGoApp
+//
+//  Created by Turgay KIRKIL on 7.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
